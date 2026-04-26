039dd0bcba1e3971474dbb92127df676fd8d0c2f
Ben4862 <btareau@gmail.com>
2026-04-27 00:20:10 +0200

    Update anchorFile.js

--- src/routes/anchorFile.js
+++ src/routes/anchorFile.js
@@ -24,16 +24,30 @@ async function signHash(hexHash) {
 anchorFileRouter.post("/", upload, async function(req, res, next) {
   try {
     var buffer;
-    var filename;
+    var filename = "capture.jpg";
+
+    console.log("files:", req.files ? req.files.length : 0);
+    console.log("body keys:", Object.keys(req.body || {}));
+    console.log("content-type:", req.headers["content-type"]);
+
     if (req.files && req.files.length > 0) {
       buffer = req.files[0].buffer;
       filename = req.files[0].originalname || "capture.jpg";
     } else if (req.body && req.body.data) {
       buffer = Buffer.from(req.body.data, "base64");
-      filename = req.body.filename || "capture.jpg";
-    } else {
+    } else if (req.body && Object.keys(req.body).length > 0) {
+      var firstKey = Object.keys(req.body)[0];
+      var val = req.body[firstKey];
+      if (typeof val === "string" && val.length > 100) {
+        buffer = Buffer.from(val, "base64");
+      }
+    }
+
+    if (!buffer) {
+      console.log("no buffer found");
       return res.status(400).json({ error: "no_file" });
     }
+
     var hexHash = hashBuffer(buffer);
     var sig = await signHash(hexHash);
     var metadata = {
@@ -51,10 +65,4 @@ anchorFileRouter.post("/", upload, async function(req, res, next) {
       anchoredAt: new Date(result.anchoredAt * 1000).toISOString(),
       signer: sig.signer,
       filename: filename,
-      verifyUrl: qr.url,
-      qrDataUrl: qr.qrDataUrl
-    });
-  } catch (err) {
-    next(err);
-  }
-});
+      verifyUrl
