ccfa19b7606bd0415e40064705eaea27237c31f7
Ben4862 <btareau@gmail.com>
2026-04-27 22:29:09 +0200

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -3,11 +3,9 @@ const { ethers } = require("hardhat");
 async function main() {
   const [deployer] = await ethers.getSigners();
   console.log("Deploying from:", deployer.address);
-
   const Factory = await ethers.getContractFactory("EvidenceAnchor");
   const contract = await Factory.deploy();
   await contract.waitForDeployment();
-
   const address = await contract.getAddress();
   console.log("Contract deployed to:", address);
 }
