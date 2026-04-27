3ca3bafcd7d81d877d0ecf2f50e2f7fada1169de
Ben4862 <btareau@gmail.com>
2026-04-27 22:25:05 +0200

    Update hardhat.config.cjs

--- hardhat.config.cjs
+++ hardhat.config.cjs
@@ -1,10 +1,10 @@
 require("@nomicfoundation/hardhat-ethers");
-require("dotenv").config();
 
 module.exports = {
   solidity: "0.8.20",
   networks: {
     polygon: {
+      type: "http",
       url: "https://polygon-rpc.com",
       accounts: [process.env.DEPLOYER_PRIVATE_KEY],
       chainId: 137,
