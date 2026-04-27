0f78eb0cfee913fd2607255bf819177025e8f829
Ben4862 <btareau@gmail.com>
2026-04-27 23:16:36 +0200

    Update qr.js

--- src/services/qr.js
+++ src/services/qr.js
@@ -1,15 +1,12 @@
 import QRCode from "qrcode";
-
 export async function generateVerifyQR(hexHash) {
-  const baseUrl = process.env.VERIFY_PORTAL_URL || "https://verify.verdchain.com";
-  const url     = `${baseUrl}/${hexHash}`;
-
+  const baseUrl = process.env.VERIFY_PORTAL_URL || "https://ben4862.github.io/verdchain?hash=";
+  const url     = `${baseUrl}${hexHash}`;
   const dataUrl = await QRCode.toDataURL(url, {
     errorCorrectionLevel: "H",
     type:   "image/png",
     margin: 2,
     width:  300,
   });
-
   return { url, qrDataUrl: dataUrl };
 }
